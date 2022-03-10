ac4d12175276681acef206a1137f18ab09e090d5
adelkaff <adelkaff20@gmail.com>
2022-03-10 16:34:45 +0000

    Test

--- main.cpp
+++ main.cpp
@@ -1,14 +1,6 @@
-#include <cstdio>
-#include <array>
+#include <iostream>
 
 int main()
 {
-        std::array<int,5> x = {2, 4, 6, 8, 10};
-        std::array<int,5> y = {1, 3, 5, 7, 9};
-        for (int i = 0; i < x.size(); i+=1)
-        {
-            x[i] += y[i];
-            printf("sum[%d] = %d\n", i, x[i]);
-        }
-
+        std::cout << "TESSSSSST";
 }
